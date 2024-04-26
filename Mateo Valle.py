bebbfc923c9b0be9e2a2973b57a928341aa4e58d
matstyle <matcambisaca@gmail.com>
2024-04-25 20:59:23 -0500

    MATEO VALLE

---
+++ Mateo Valle.py
@@ -0,0 +1,8 @@
+numero = int(input("Escribe un: "))  # input ingresar por teclado
+if numero > 5:# si
+    print('Es mayor a 5')
+elif numero == 5:
+    print("Es = a 5") # si es numero == 5
+else: # Si no es 
+    print("Es menor a 5")
+    
