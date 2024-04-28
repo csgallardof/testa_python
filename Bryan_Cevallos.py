ece0b3d73e7add3a11041fa335ee7c9c52694d66
Bcevallosh <alternacevallos@gmail.com>
2024-04-27 23:12:52 -0500

    Bryan Cevallos

---
+++ Bryan_Cevallos.py
@@ -0,0 +1,10 @@
+numero= int (input('Ingrese un numero: '))
+
+if numero > 5:
+    print('El numero es mayor a 5')
+elif numero ==5:
+    print('El numero es igual a 5')   
+
+else:
+    print('el numero es menor a 5')
+    
