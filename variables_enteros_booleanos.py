47fad3c2051b605189c128a423e29c14c1dd44d9
rsuntasig <rsuntasig@estud.tesa.edu.ec>
2024-04-30 21:21:14 -0500

    Ejemplos de variables int, string and boolean

---
+++ variables_enteros_booleanos.py
@@ -0,0 +1,21 @@
+#variables String
+my_name_01="Carlos"
+my_name='Juan'
+
+#print('Este es mi nombre:=>',my_name)
+
+my_name="Pedro"
+
+print('Este es mi nombre variable renombrada=>',my_name)
+
+#inte
+
+my_age=41
+print('Esta es mi edad=>',my_age,'y mi nombre es:',my_name_01)
+
+
+#Ejemplo de impresión de mi edad
+
+my_age_you=input('Cuál es tú edad?: ')
+print('Tú edad es: ',my_age_you)
+print(type(my_age_you))
