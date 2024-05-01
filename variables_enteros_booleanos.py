881ce4fdb360ec00c83e91b14e89cfcc574e9758
Carlos G <carlos@urbadigital.com>
2024-04-30 21:21:06 -0500

    Ejemplos de variables int, string and boolean

---
+++ variables_enteros_booleanos.py
@@ -0,0 +1,24 @@
+# # variables String
+
+# my_name_01 = "Carlos"
+# my_name = 'Juan'
+
+# #print('Este es mi nombre:=>',my_name)
+
+# my_name = "Pedro"
+
+# print('Este es mi nombre varible renombrada=>',my_name)
+
+# #integer
+
+# my_age = 41
+# print('Esta es mi edad: ', my_age,' y mi nombre es: ' ,my_name_01 )
+
+
+# Ejemplo de impresion de mi edad
+
+my_age_you= input('¿Cuál es tu edad?')
+print('Tu edad es: ', my_age_you)
+print(type(my_age_you))
+
+
