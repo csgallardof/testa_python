7d9ff1fa9eda6e57e524dc61f02b06b6f60c1138
rsuntasig <rsuntasig@estud.tesa.edu.ec>
2024-05-02 20:36:23 -0500

    listas

---
+++ listas.py
@@ -0,0 +1,20 @@
+numeros=[1,2,45,3]
+
+print(numeros)
+
+print(type(numeros))
+
+asignaturas=['matematicas','cultura del vino']
+print(asignaturas)
+print(type(asignaturas))
+
+tipo_de_datos=[1,True,'cadena de caracteres']
+print(tipo_de_datos)
+print(type(tipo_de_datos))
+
+print(numeros[2]) ## Se imprime el valor de la posición de la lista
+
+asignaturas[1]='Python'
+print(asignaturas)
+
+print(numeros.index(45))
