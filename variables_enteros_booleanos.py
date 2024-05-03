554a834a53d0afae4aceb8f668717c6f317be850
rsuntasig <rsuntasig@estud.tesa.edu.ec>
2024-05-02 19:20:17 -0500

    enteros

--- variables_enteros_booleanos.py
+++ variables_enteros_booleanos.py
@@ -1,21 +1,30 @@
 #variables String
-my_name_01="Carlos"
-my_name='Juan'
+#my_name_01="Carlos"
+#my_name='Juan'
 
 #print('Este es mi nombre:=>',my_name)
 
-my_name="Pedro"
+#my_name="Pedro"
 
-print('Este es mi nombre variable renombrada=>',my_name)
+#print('Este es mi nombre variable renombrada=>',my_name)
 
 #inte
 
-my_age=41
-print('Esta es mi edad=>',my_age,'y mi nombre es:',my_name_01)
+#my_age=41
+#print('Esta es mi edad=>',my_age,'y mi nombre es:',my_name_01)
 
 
 #Ejemplo de impresión de mi edad
 
-my_age_you=input('Cuál es tú edad?: ')
-print('Tú edad es: ',my_age_you)
-print(type(my_age_you))
+#my_age_you=input('Cuál es tú edad?: ')
+#print('Tú edad es: ',my_age_you)
+#print(type(my_age_you))
+
+#Ejemplo de variables boleanas
+is_true=True
+print(type(is_true))
+print('Valor 1 de is_true=>',is_true)
+##Comentario: renombro a la variable is_true
+is_true=False
+print(type(is_true))
+print('Valor 2 de is_true=>',is_true)
