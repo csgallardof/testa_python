7943cf23a737adbb59c903a4b5b5a67693a0293b
Bcevallosh <alternacevallos@gmail.com>
2024-05-02 19:41:25 -0500

    algebra boleana BC

---
+++ condicionales_BryanCevallos.py
@@ -0,0 +1,40 @@
+# > Mayor q
+print('Ejemplos de mayor que')
+print(7>6)
+print(6>7)
+print(7>7)
+# < Menor que
+print('Ejemplos de menor que')
+print(10<50)
+print(50<10)
+print(50<50)
+# >= Mayor o igual
+print('Ejemplos de mayor igual')
+print(30>=10)
+print(30>=30)
+print(10>=30)
+# >= Menor o igual
+print('Ejemplos de menor igual')
+print(30<=10)
+print(30<=30)
+print(10<=30)
+# == Igual Igual
+print('Ejemplos de igual igual')
+print(10==10)
+print(0==0)
+# != Diferencia
+print('Ejemplos de diferencia')
+print(10!=10)
+print(100!=0)
+#Cadena de caracteres
+print('Ejemplos de caracteres')
+print('Carlos'=='Carlos')
+print('Karlos'=='Carlos')
+
+print('Ejemplos de cariables y condicionales')
+my_age=26
+print(my_age>18)
+
+
+
+
