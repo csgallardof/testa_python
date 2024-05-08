74b23624b2320c741462178b3ea0721291617204
Carlos G <carlos@urbadigital.com>
2024-05-07 22:07:02 -0500

    fix tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,29 +1,26 @@
-numbers = (1,4,5,6,7,8)
-strings = ('Uno', 'Dos', 'Dos', 'Cuatro','Dos')
+numbers = [1,4,5,6,7,8]
+strings = ('Uno','Dos','Tres','Cuatro')
 
 print(numbers)
 
-print('2=> ', numbers[2])
+print('2=>',numbers[2])
 
 print(type(numbers))
 
-print('3=> ', strings[3])
+print('3=>',strings[3])
 print(type(strings))
-
-
 
 print(strings.index('Dos'))
 
 print(strings.count('Dos'))
 
 
+###Tupla en una lista
 
-### Tupla en una lista
-
-list_tupla = list(numbers)
+list_tupla=list(numbers)
 print(list_tupla)
 print(type(list_tupla))
 
-new_tupla = tuple(list_tupla)
+new_tupla=tuple(list_tupla)
 print(new_tupla)
 print(type(new_tupla))
